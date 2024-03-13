26997fd59cfe8c385b5c88b885d2050204526bb1
Takashicc <105186894+Takashicc@users.noreply.github.com>
2024-03-13 18:07:32 +0900

    refactor: Switch from environment variables to inputs for action parameters

---
+++ src/helper/github_info_helper.ts
@@ -0,0 +1,29 @@
+export type GitHubInfo = {
+	serverUrl: string;
+	repo: string;
+	runId: string;
+	actionUrl: string;
+	workflow: string;
+	runnerOS: string;
+	actor: string;
+};
+
+export function getGitHubInfo(): GitHubInfo {
+	const serverUrl = process.env.GITHUB_SERVER_URL || "";
+	const repo = process.env.GITHUB_REPOSITORY || "";
+	const runId = process.env.GITHUB_RUN_ID || "";
+	const actionUrl = `${serverUrl}/${repo}/actions/runs/${runId}`;
+	const workflow = process.env.GITHUB_WORKFLOW || "";
+	const runnerOS = process.env.RUNNER_OS || "";
+	const actor = process.env.GITHUB_ACTOR || "";
+
+	return {
+		serverUrl,
+		repo,
+		runId,
+		actionUrl,
+		workflow,
+		runnerOS,
+		actor,
+	};
+}
